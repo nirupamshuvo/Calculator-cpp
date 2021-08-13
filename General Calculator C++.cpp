7a317a7ed709a6956490ab8c893077dbeb9ca77c
Nirupam Shuvo <39645954+nirupamshuvo@users.noreply.github.com>
2021-08-13 13:42:37 +0600

    Add files via upload

---
+++ General Calculator C++.cpp
@@ -0,0 +1,80 @@
+#include<iostream>
+using namespace std;
+int main (){
+
+int a,b,result;
+
+cout <<"Enter the value of 1st number : ";
+cin>> a;
+
+cout<<"Enter the value of 2nd number : ";
+cin>>b;
+
+cout<<endl;
+
+result=a+b;
+
+cout<<"The result of this equation : "<< result <<endl;
+}
+
+
+/*
+
+cout <<"Enter the value of 1st number : ";
+cin>> a;
+
+cout<<"Enter the value of 2nd number : ";
+cin>>b;
+
+cout<<endl;
+
+result=a-b;
+
+cout<<"The result of this equation : "<< result <<endl;
+}
+
+*/
+/*
+cout <<"Enter the value of 1st number : ";
+cin>> a;
+
+cout<<"Enter the value of 2nd number : ";
+cin>>b;
+
+cout<<endl;
+
+result=a*b;
+
+cout<<"The result of this equation : "<< result <<endl;
+}
+*/
+/*
+
+cout <<"Enter the value of 1st number : ";
+cin>> a;
+
+cout<<"Enter the value of 2nd number : ";
+cin>>b;
+
+cout<<endl;
+
+result=a/b;
+
+cout<<"The result of this equation : "<< result <<endl;
+}
+*/
+/*
+cout <<"Enter the value of 1st number : ";
+cin>> a;
+
+cout<<"Enter the value of 2nd number : ";
+cin>>b;
+
+cout<<endl;
+
+result=a%b;
+
+cout<<"The result of this equation : "<< result <<endl;
+}
+
+*/
